76bb6fc1a5b6f2c05ee065bc686ab77f88ab6c1f
Aaron Cohn <aacohn84@gmail.com>
2014-10-14 15:31:25 -0700

    Rename User to Employee

--- app/models/NoSuchEmployeeException.java
+++ app/models/NoSuchEmployeeException.java
@@ -5,6 +5,6 @@ public class NoSuchUserException extends RuntimeException {
 	private static final long serialVersionUID = -924876106897727642L;
 
 	public NoSuchUserException(String username) {
-		super("User [ " + username + " ] not found.");
+		super("Employee [ " + username + " ] not found.");
 	}
 }
