864dba6e4339e83d1eed13d6266909960dac9fcc
Aaron Cohn <aacohn84@gmail.com>
2014-10-22 17:06:20 -0700

    Implement deserialization of decisionTree
    Had to make changes to serialization of the tree to make this work
    properly. For each SerializedDecision, we are now storing the contextld
    of the next and previous Decision, and we are also storing the contextld
    of firstDecision and mostRecentlyMadeDecision, so we can easily
    reconstruct the tree.

--- app/models/DecisionTree.java
+++ app/models/DecisionTree.java
@@ -1,12 +1,14 @@
 package models;
 
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
 import play.libs.Json;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
 import core.forms.CMSForm;
 import core.tree.Node;
 
@@ -25,13 +27,13 @@ import core.tree.Node;
  * a complete path through the tree. This is referred to as the active path, and
  * there can only be one in the tree at any given time.
  * </p>
- * 
+ *
  * @author Aaron Cohn
  */
 public class DecisionTree implements Iterable<Decision> {
 	/**
 	 * Traverses the active path through the DecisionTree.
-	 * 
+	 *
 	 * @author Aaron Cohn
 	 */
 	public class DecisionTreeIterator implements Iterator<Decision> {
@@ -59,11 +61,18 @@ public class DecisionTree implements Iterable<Decision> {
 		}
 	}
 
-	static class SerializedDecision implements Serializable {
-		private static final long serialVersionUID = -7893808643530736707L;
+	// Variable names kept short in serialized classes to reduce JSON size.
+	static class SerializedDecision {
+		public String c; // context id
+		public String p; // previous context id
+		public String n; // next context id
+		public String i; // serialized input
+	}
 
-		public String contextId;
-		public String serializedInput;
+	static class SerializedDecisionTree {
+		public String f; // context id of first decision
+		public String r; // context id of most recently made decision
+		public SerializedDecision[] d; // serialized decisions
 	}
 
 	Map<String, Decision> decisions;
@@ -148,14 +157,50 @@ public class DecisionTree implements Iterable<Decision> {
 
 	public String serialize() {
 		ArrayList<SerializedDecision> list = new ArrayList<>();
-		for (Decision decision : decisions.values()) {
+		for (Decision d : decisions.values()) {
 			SerializedDecision sd = new SerializedDecision();
-			sd.contextId = decision.context.id;
-			sd.serializedInput = decision.serializedInput;
+			sd.c = d.context.id;
+			sd.p = d.previous != null ? d.previous.context.id : null;
+			sd.n = d.next != null ? d.next.context.id : null;
+			sd.i = d.serializedInput;
 			list.add(sd);
 		}
-		SerializedDecision[] array = new SerializedDecision[list.size()];
-		array = list.toArray(array);
-		return Json.toJson(array).toString();
+		SerializedDecisionTree sdt = new SerializedDecisionTree();
+		sdt.f = firstDecision.context.id;
+		sdt.r = mostRecentlyMadeDecision.context.id;
+		sdt.d = list.toArray(new SerializedDecision[list.size()]);
+		return Json.toJson(sdt).toString();
+	}
+
+	public void deserialize(String serializedDecisions) {
+		JsonNode jsonNode = Json.parse(serializedDecisions);
+		SerializedDecisionTree sdt = Json.fromJson(jsonNode,
+				SerializedDecisionTree.class);
+		for (SerializedDecision sd : sdt.d) {
+			Decision d = decisionToDeserialize(sd.c);
+			d.previous = decisionToDeserialize(sd.p);
+			d.next = decisionToDeserialize(sd.n);
+			d.serializedInput = sd.i;
+		}
+		firstDecision = getDecision(sdt.f);
+		mostRecentlyMadeDecision = getDecision(sdt.r);
+	}
+
+	/*
+	 * Deserialization helper -- returns the decision that belongs to the given
+	 * context. If it doesn't exist, it will be created and stored for future
+	 * retrieval. The only member field guaranteed to be filled is the context.
+	 */
+	private Decision decisionToDeserialize(String contextId) {
+		if (contextId == null) {
+			return null;
+		}
+		Decision decision = getDecision(contextId);
+		if (decision == null) {
+			decision = new Decision();
+			decision.context = form.getNode(contextId);
+			decisions.put(contextId, decision);
+		}
+		return decision;
 	}
 }
